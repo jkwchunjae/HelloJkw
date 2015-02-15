28efdb128bc4e30a0854c3039540b788dc083dcd
jkwchunjae <jkwchunjae@daum.net>
2015-02-16 00:10:23 +0900

    Nancy Project 추가, Index 추가
    Nancy 를 nuget 으로 받지 않은 이유는.. 받으면 이상하게 static contents 인식이 안된다.. 모르겠다..ㅜㅜ

--- helloJkw/Program.cs
+++ helloJkw/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+using Nancy.Conventions;
 
 namespace helloJkw
 {
@@ -10,6 +13,11 @@ namespace helloJkw
 	{
 		static void Main(string[] args)
 		{
+			using (var host = new NancyHost(new Uri("http://localhost:80")))
+			{
+				host.Start();
+				Console.ReadLine();
+			}
 		}
 	}
 }
---
+++ helloJkw/modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+public class IndexModule : NancyModule
+{
+	public IndexModule()
+	{
+		Get["/"] = _ =>
+		{
+			return View["index"];
+		};
+	}
+}
