1af5e95102121fb708f098b397a955ba4e9c036c
jkwchunjae <jkwchunjae@daum.net>
2015-04-03 08:15:12 +0900

    blog 시작..
    아무것도 없는상태..

---
+++ helloJkw/Modules/Jkw/JkwBlogModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloJkw.Modules.Jkw
+{
+	public class JkwBlogModule : NancyModule
+	{
+		public JkwBlogModule()
+		{
+			Get["/blog"] = _ => 
+			{
+				return "blog";
+			};
+		}
+	}
+}
