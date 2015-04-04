bc494ba7fa37ad40d8d0195bf286ac47acb75101
jkwchunjae <jkwchunjae@daum.net>
2015-04-04 21:07:55 +0900

    PostManager 추가, 코딩 중..
    BlogManager 로 개명 예정!

---
+++ helloJkw/Jkw/Blog/PostManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions;
+using System.IO;
+
+namespace helloJkw.Jkw.Blog
+{
+	public static class PostManager
+	{
+		public static string BlogRootPath = @"jkw/blog/";
+		public static List<Post> Posts;
+
+		public static void UpdatePost()
+		{
+			var path = Path.Combine(Environment.CurrentDirectory, 
+			Directory.GetFiles()
+		}
+	}
+}
