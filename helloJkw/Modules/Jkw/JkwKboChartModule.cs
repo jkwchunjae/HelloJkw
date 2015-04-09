21f7270072fce2823ad6416bc4ee0e93c9c129d6
jkwchunjae <jkwchunjae@daum.net>
2015-04-09 18:05:07 +0900

    highchart 추가

---
+++ helloJkw/Modules/Jkw/JkwKboChartModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloJkw.Modules.Jkw
+{
+	public class JkwKboChartModule : NancyModule
+	{
+		public JkwKboChartModule()
+		{
+			Get["/kbochart"] = _ =>
+			{
+				return View["jkwKboChart"];
+			};
+		}
+	}
+}
