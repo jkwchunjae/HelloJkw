6e2fe9987626124ff1e1e35946c5403c54c1b489
jkwchunjae <jkwchunjae@daum.net>
2015-11-25 00:36:05 +0900

    Diary 시작

---
+++ helloJkw/Modules/Jkw/JkwDiaryModule.cs
@@ -0,0 +1,32 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+using Extensions;
+using helloJkw.Utils;
+using System.Diagnostics;
+using System.IO;
+
+namespace helloJkw
+{
+	public class JkwDiaryModule : JkwModule
+	{
+		public JkwDiaryModule()
+		{
+			Get["/diary"] = _ =>
+			{
+				if (session.IsLogin)
+				{
+					return View["diary/jkwDiaryHome", Model];
+				}
+				else
+				{
+					return View["diary/jkwDiaryRequireLogin", Model];
+				}
+			};
+		}
+	}
+}
