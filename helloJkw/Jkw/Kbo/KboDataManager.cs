021a3b714e59e3215515ffcf0460a9c931549505
jkwchunjae <jkwchunjae@daum.net>
2016-02-13 19:05:02 +0900

    [KBO] KBO 홈페이지 변경 대응

--- helloJkw/Jkw/Kbo/KboDataManager.cs
+++ helloJkw/Jkw/Kbo/KboDataManager.cs
@@ -72,14 +72,42 @@ namespace helloJkw
 			try
 			{
 				#region ReadHtml
-				var url = "http://www.koreabaseball.com/Schedule/ScoreBoard/ScoreBoard.aspx?gameDate={0}".With(date);
-				var webRequest = WebRequest.Create(url);
-				var response = webRequest.GetResponse();
-				string html;
-				using (var reader = new StreamReader(response.GetResponseStream()))
+				var data = new Dictionary<string, string>();
+				data["__ASYNCPOST"] = @"true";
+				data["__EVENTARGUMENT"] = @"";
+				data["__EVENTTARGET"] = @"ctl00%24ctl00%24cphContainer%24cphContents%24btnCalendarSelect";
+				data["__EVENTVALIDATION"] = @"%2FwEdAAb%2BoIlBCtNTRD6wl%2BbqGE%2FpEG4FQbqeMQMc81CqbOAKuzLjeDrPSz9QBWoiuGg97q%2FJoS5f1T9NhbVQDKOc%2BSILE4seC6JQTftOIuy7Cku2br0XPyMYYAm84yfpHD9ObZqPnKjnezLQAQzYIQeH57lZuNKhlEJd6KGbi9me1bHt3A%3D%3D";
+				data["__VIEWSTATE"] = @"%2FwEPDwULLTEwNjQ3MjM0NTAPZBYCZg9kFgJmD2QWAgIDD2QWBgIBDxYCHgRUZXh0BQoy7JuUIDEz7J28ZAICDxYCHgtfIUl0ZW1Db3VudAIBFgJmD2QWAmYPFQQKMjAxMy0wMi0xMwIxMxcyMDEz64WEIO2TqOyymOyKpOumrC4uLiI07JuUIDLsnbwg6rCc66eJLiDrtoHrtoDrpqzqt7ggLi4uZAIDD2QWAgIDD2QWAgIDD2QWAmYPZBYGAgEPDxYCHwAFDzIwMTYuMDIuMTMo7YagKWRkAgMPFgIfAWZkAgQPDxYCHgdWaXNpYmxlZ2RkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYCBS9jdGwwMCRjdGwwMCRjcGhDb250YWluZXIkY3BoQ29udGVudHMkYnRuUHJlRGF0ZQUwY3RsMDAkY3RsMDAkY3BoQ29udGFpbmVyJGNwaENvbnRlbnRzJGJ0bk5leHREYXRlv9eyPfeyr7XQvXeI89UEILGbhWDknZTv6j7044gPr%2B0%3D";
+				data["__VIEWSTATEGENERATOR"] = @"50BA479B";
+				data["ctl00%24ctl00%24cphContainer%24cphContents%24hfSearchDate"] = date.ToString();
+				data["ctl00%24ctl00%24cphContainer%24ScriptManager1"] = @"ctl00%24ctl00%24cphContainer%24cphContents%24udpRecord%7Cctl00%24ctl00%24cphContainer%24cphContents%24btnCalendarSelect";
+				data["ctl00%24ctl00%24txtSearchWord"] = @"";
+
+				var param = data.Select(x => "{0}={1}".With(x.Key, x.Value)).StringJoin("&");
+				var paramBytes = Encoding.ASCII.GetBytes(param);
+
+				var request = WebRequest.CreateHttp(@"http://www.koreabaseball.com/Schedule/ScoreBoard/ScoreBoard.aspx");
+
+				request.Method = "POST";
+				request.Accept = @"*/*";
+				//request.Connection = "Keep-Alive";
+				request.ContentType = @"application/x-www-form-urlencoded; charset=utf-8";
+				request.Host = @"www.koreabaseball.com";
+				request.CookieContainer = new CookieContainer();
+				request.CookieContainer.Add(new Cookie("ASP.NET_SessionId", "qnfu3aumr5wiwtcwkgglw5ui") { Domain = request.Host });
+				request.CookieContainer.Add(new Cookie("_ga", "GA1.2.610003585.1455356327") { Domain = request.Host });
+				request.CookieContainer.Add(new Cookie("_gat", "1") { Domain = request.Host });
+				request.UserAgent = @"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; Win64; x64; Trident/8.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2; Media Center PC 6.0; Tablet PC 2.0)";
+				request.Referer = @"http://www.koreabaseball.com/Schedule/ScoreBoard/ScoreBoard.aspx";
+
+				using (var stream = request.GetRequestStream())
 				{
-					html = reader.ReadToEnd();
+					stream.Write(paramBytes, 0, paramBytes.Length);
 				}
+
+				var response = (HttpWebResponse)request.GetResponse();
+
+				var html = new StreamReader(response.GetResponseStream()).ReadToEnd();
 				#endregion
 
 				var htmlDoc = new HtmlDocument();
