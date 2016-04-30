a99f6741d8eecf86d2b75808810b45fa854dd05f
jkwchunjae <jkwchunjae@daum.net>
2016-04-30 17:45:40 +0900

    [js-date] getDate8 기능 변경
    다음부턴 한번에 잘하자!

--- helloJkw/_Static/js/jkw/utils.js
+++ helloJkw/_Static/js/jkw/utils.js
@@ -1,4 +1,8 @@
 ﻿function getDate8(date /* moment object */) {
+	return date.year() * 10000 + (date.month() + 1) * 100 + date.date();
+}
+
+function getDateDot(date /* moment object */) {
 	return date.year() + '.' + padStr(date.month() + 1) + '.' + padStr(date.date());
 }
 
