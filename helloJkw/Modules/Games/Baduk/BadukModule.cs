3125b7e5d8f7d229bec9fac299ba3223e271eec5
jkwchunjae <jkwchunjae@daum.net>
2016-06-26 11:22:25 +0900

    [바둑] 첫 커밋
    일단 돌 놓는 기능만

---
+++ helloJkw/Modules/Games/Baduk/BadukModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloJkw
+{
+	public class BadukModule : JkwModule
+	{
+		public BadukModule()
+		{
+			Get["/games/Baduk"] = _ =>
+			{
+				return View["Games/Baduk/badukMain.cshtml", Model];
+			};
+		}
+	}
+}
