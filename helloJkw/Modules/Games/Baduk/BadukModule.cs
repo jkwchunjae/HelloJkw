7ad87a8dcb2394d905569664b74a4397ee364c4c
jkwchunjae <jkwchunjae@daum.net>
2016-06-28 00:15:54 +0900

    [바둑] 메모 추가

--- helloJkw/Modules/Games/Baduk/BadukModule.cs
+++ helloJkw/Modules/Games/Baduk/BadukModule.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,22 @@ namespace helloJkw
 {
 	public class BadukModule : JkwModule
 	{
+		string pathMemo = @"jkw/games/Baduk/memo.txt";
 		public BadukModule()
 		{
 			Get["/games/Baduk"] = _ =>
 			{
+				Model.Memo = JsonConvert.DeserializeObject<string>(File.ReadAllText(pathMemo, Encoding.UTF8));
 				return View["Games/Baduk/badukMain.cshtml", Model];
+			};
+
+			Post["/games/Baduk/save-memo"] = _ =>
+			{
+				if (!session.IsLogin)
+					return "로그인하여야 합니다.";
+				string memo = Request.Form["memo"];
+				File.WriteAllText(pathMemo, JsonConvert.SerializeObject(memo), Encoding.UTF8);
+				return "저장되었습니다.";
 			};
 		}
 	}
